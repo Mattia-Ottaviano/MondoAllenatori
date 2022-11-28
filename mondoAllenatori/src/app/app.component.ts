c031f8c16ad3e281f8afd5052590de1b266913ff
Mattia-Ottaviano <ottaviano.mattia@iisgalvanimi.edu.it>
2022-11-28 10:54:59 +0000

    .

--- mondoAllenatori/src/app/app.component.ts
+++ mondoAllenatori/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mondoAllenatori';
+  obs!: Observable<any>; ///TODO: Fix data type
+  storage: any;
+  constructor(private http: HttpClient) {
+    this.obs = this.http.get('https://3245-mattiaottav-mondoallena-8qnlyt4dxj6.ws-eu77.gitpod.io/pippo')
+    this.obs.subscribe(this.getdata);
+  }
+
+  getdata = (data: any) => {
+    this.storage = data;
+    console.log(this.storage)
+  }
+
+
+  onClick(user: HTMLInputElement, pwd: HTMLInputElement): boolean {
+    let values = JSON.stringify({
+      user: user.value,
+      pwd: pwd.value,
+    })
+
+    console.log(values)
+    this.http.post('https://3245-mattiaottav-mondoallena-8qnlyt4dxj6.ws-eu77.gitpod.io/pluto', values, )
+      .subscribe(() => { console.log("dati inviati con successo") });
+
+    return false
+  }
 }
--- mondoAllenatori/src/app/app.module.ts
+++ mondoAllenatori/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogRegComponent } from './log-reg/log-reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogRegComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
